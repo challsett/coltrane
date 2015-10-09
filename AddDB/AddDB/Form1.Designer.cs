47fd0989d38c71b60b5f7f9c76f6ff06624c6531
challsett <challsett@live.com>
2015-10-09 18:29:36 -0400

    Create a database with Permissions
    Enables you to create and delete a database from SQL Server

---
+++ AddDB/AddDB/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace AddDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxDB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkedListBoxPermissions = new System.Windows.Forms.CheckedListBox();
+            this.comboBoxDatabases = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.Location = new System.Drawing.Point(16, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CreateDB";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.Location = new System.Drawing.Point(297, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Delete DB";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxDB
+            // 
+            this.textBoxDB.Location = new System.Drawing.Point(16, 111);
+            this.textBoxDB.Name = "textBoxDB";
+            this.textBoxDB.Size = new System.Drawing.Size(141, 20);
+            this.textBoxDB.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter DB to Create ";
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(16, 150);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(141, 20);
+            this.textBoxUser.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Enter user name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(296, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter DB to delete";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Select Permissions";
+            // 
+            // checkedListBoxPermissions
+            // 
+            this.checkedListBoxPermissions.FormattingEnabled = true;
+            this.checkedListBoxPermissions.Location = new System.Drawing.Point(16, 189);
+            this.checkedListBoxPermissions.Name = "checkedListBoxPermissions";
+            this.checkedListBoxPermissions.Size = new System.Drawing.Size(141, 79);
+            this.checkedListBoxPermissions.TabIndex = 10;
+            // 
+            // comboBoxDatabases
+            // 
+            this.comboBoxDatabases.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxDatabases.FormattingEnabled = true;
+            this.comboBoxDatabases.Location = new System.Drawing.Point(297, 111);
+            this.comboBoxDatabases.Name = "comboBoxDatabases";
+            this.comboBoxDatabases.Size = new System.Drawing.Size(141, 21);
+            this.comboBoxDatabases.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft PhagsPa", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Maroon;
+            this.label5.Location = new System.Drawing.Point(126, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(207, 31);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Candice\'s DB Tool";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label6.Location = new System.Drawing.Point(12, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(144, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Create a Database";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label7.Location = new System.Drawing.Point(295, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Delete a Database";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 325);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxDatabases);
+            this.Controls.Add(this.checkedListBoxPermissions);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDB);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxDB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckedListBox checkedListBoxPermissions;
+        private System.Windows.Forms.ComboBox comboBoxDatabases;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ AddDB/AddDB/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Management.Smo;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace AddDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            populateListViewPermissions();
+            populateDatabases();
+        }
+
+        private void populateDatabases()
+        {
+            var serverName = ".";
+            Server svr = new Server(serverName);
+            foreach (Database db in svr.Databases) //delete all databases with entered name
+            {
+                if(db.Name.ToLower() != "master") //do not include master db
+                {
+                    comboBoxDatabases.Items.Add(db.Name);
+                }
+            }
+        }
+
+        private void populateListViewPermissions()
+        {
+            checkedListBoxPermissions.Items.Add("db_owner");
+            checkedListBoxPermissions.Items.Add("db_accessadmin");
+            checkedListBoxPermissions.Items.Add("db_backupoperator");
+            checkedListBoxPermissions.Items.Add("db_datareader");
+            checkedListBoxPermissions.Items.Add("db_datawriter");
+            checkedListBoxPermissions.Items.Add("db_ddladmin");
+            checkedListBoxPermissions.Items.Add("db_securityadmin");
+        }
+
+        private void button1_Click(object sender, EventArgs e) //add db
+        {
+            var dbName = textBoxDB.Text;
+            string userName = textBoxUser.Text;
+            var serverName = "."; // SQL Server Instance name
+
+            Server svr = new Server(serverName);
+            var db = new Database(svr, dbName);
+            db.Create();
+
+            if (db != null)
+            {
+                // In case I need to add a login to sql
+                //Login login = new Login(svr, "beacon");
+                //login.DefaultDatabase = "master"; // Logins typically have master as default database
+                //login.LoginType = LoginType.SqlLogin;
+                //login.Create("foobar", LoginCreateOptions.None); // Enter a suitable password
+                //login.Enable();
+
+                if(userName != "")
+                {
+                    User user = new User(db, userName);
+                    user.UserType = UserType.SqlLogin;
+                    user.Login = userName;
+                    user.Create();
+
+                    List<object> listOfPermissions = checkedListBoxPermissions.CheckedItems.OfType<object>().ToList();
+                    foreach(var p in listOfPermissions)
+                    {
+                        user.AddToRole(p.ToString());
+                    }
+                }
+              
+            }
+            comboBoxDatabases.Items.Clear();
+            populateDatabases();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //delete db
+        {
+            var serverName = "."; 
+            Server svr = new Server(serverName);
+            var dbToDrop = comboBoxDatabases.SelectedItem.ToString();//textBoxDBToDrop.Text;
+            List<Database> databasesToDelete = new List<Database>();
+            foreach (Database db in svr.Databases) //delete all databases with entered name
+            {
+                if (db.Name == dbToDrop)
+                {
+                    databasesToDelete.Add(db);
+                }
+            }
+            databasesToDelete.ForEach(x =>
+            {
+                if (x.ActiveConnections > 0) //close all connections and drop db
+                {
+                    string connectionString = svr.ConnectionContext.ToString();
+
+                    System.Data.SqlClient.SqlConnectionStringBuilder builder = new System.Data.SqlClient.SqlConnectionStringBuilder();
+                    builder["Data Source"] = ".";
+                    builder["integrated Security"] = true;
+                    builder["Initial Catalog"] = dbToDrop;
+
+                    SqlConnection sqlConn = new SqlConnection(builder.ToString());
+                    Microsoft.SqlServer.Management.Common.ServerConnection serverConn = new Microsoft.SqlServer.Management.Common.ServerConnection(sqlConn);
+                    Server svrSql = new Server(serverConn);
+
+                    sqlConn.Open();
+                    String sqlCOmmandText = @"
+                             USE master
+                             ALTER DATABASE " + dbToDrop + @" SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+                             DROP DATABASE [" + dbToDrop + "]";
+
+                    SqlCommand sqlCommand = new SqlCommand(sqlCOmmandText, sqlConn);
+                    sqlCommand.ExecuteNonQuery();
+                    sqlConn.Close();
+                }
+                else
+                {
+                    x.Drop();
+                }                         
+            });
+
+            comboBoxDatabases.Items.Clear();
+            populateDatabases();
+        }
+
+    }
+}
